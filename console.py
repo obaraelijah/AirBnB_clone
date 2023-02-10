0b2b9e83929452ecdf36c621ae94c6119531c8d3
elijah obara <elijahobara357@gmail.com>
2023-02-10 11:30:37 +0300

    Modified console file

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ from models.engine.file_storage import FileStorage
 from models.user import User
 from models.base_model import BaseModel
 from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 storage = FileStorage() 
 
@@ -20,7 +25,7 @@ class HBNBCommand(cmd.Cmd):
     """
     
     prompt = '(hbnb)'
-    class_present=['User', 'BaseModel', 'State']
+    class_present=['User', 'BaseModel', 'State', 'Amenity', 'Place', 'Review', 'City']
     
     def do_quit(self, arg):
         """
