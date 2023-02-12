ef1152651f3186f8444d7466e448ea02aa0cc5bb
elijah obara <elijahobara357@gmail.com>
2023-02-12 18:26:26 +0300

    Hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module for FileStorage class."""
+"""
+Module for FileStorage class.
+"""
 import datetime
 import json
 import os
@@ -7,27 +9,37 @@ import os
 
 class FileStorage:
 
-    """Class for storing and retrieving data"""
+    """
+    Class for storing and retrieving data
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """r
+        eturns the dictionary __objects
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
     def classes(self):
-        """Returns a dictionary of valid classes and their references"""
+        """
+        Returns a dictionary of valid classes and their references
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
@@ -46,7 +58,9 @@ class FileStorage:
         return classes
 
     def reload(self):
-        """Reloads the stored objects"""
+        """
+        Reloads the stored objects
+        """
         if not os.path.isfile(FileStorage.__file_path):
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
@@ -57,7 +71,9 @@ class FileStorage:
             FileStorage.__objects = obj_dict
 
     def attributes(self):
-        """Returns the valid attributes and their types for classname"""
+        """
+        Returns the valid attributes and their types for classname
+        """
         attributes = {
             "BaseModel":
                      {"id": str,
