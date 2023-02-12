d78d3fe48dc79189cd93a00d6bc3fac0eb30e3ac
elijah obara <elijahobara357@gmail.com>
2023-02-12 18:26:56 +0300

    Hot fix at city module

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,9 @@ storage = FileStorage()
 
 class BaseModel:
 
-    """Class from which all other classes will inherit"""
+    """
+    Class from which all other classes will inherit
+    """
 
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
@@ -35,19 +37,25 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Returns official string representation"""
+        """
+        Returns official string representation
+        """
 
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
+        """
+        updates the public instance attribute updated_at
+        """
 
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
 
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
